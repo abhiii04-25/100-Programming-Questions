f965d454191094e1433f47067ce057b38382e9f7
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-07-20 21:08:23 +0530

    11

---
+++ Conditions.py
@@ -0,0 +1,6 @@
+#.Write a program to read a number and check whether it is even or odd.
+num=int(input("Enter an number: "))
+if(num%2==0):
+    print("The number is even: ",num)
+else:
+       print("The number is odd: ",num)
