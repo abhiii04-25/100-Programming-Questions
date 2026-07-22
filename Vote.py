688b820d262325db1bc8fb80324b77f735269317
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-07-22 23:51:26 +0530

    20

---
+++ Vote.py
@@ -0,0 +1,7 @@
+#Write a program to read the age of a person and check whether they are eligible to vote.
+age=int(input("Enter the age: "))
+if(age>=18):
+    print("Your are eligible to vote")
+else:
+     print("Your are not eligible to vote")
+
