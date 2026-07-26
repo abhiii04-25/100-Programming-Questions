9079314e52d34c4af641f782a90202ba17e5b07f
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-07-26 23:37:56 +0530

    29

---
+++ MuliplaofM.py
@@ -0,0 +1,6 @@
+#Write a program to display all multiples of a number m up to n terms.
+m = int(input("Enter the number: "))
+n = int(input("Enter the number of terms: "))
+
+for i in range(1, n + 1):
+    print(m * i)
