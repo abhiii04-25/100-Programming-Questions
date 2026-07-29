159c6379a211340eb241161c6f6b514a9165f12e
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-07-29 23:27:57 +0530

    31

---
+++ Count.py
@@ -0,0 +1,8 @@
+#Write a program to count the number of digits in a number n.
+n=int(input("Enter an number:"))
+count=0
+while n!=0:
+    n=n//10
+    count=count+1
+print("Number of digits: ",count)
+
