ae39551386dbea9b5a07bee121e057914f393b65
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-07-31 23:23:53 +0530

    Pascal Triangle

---
+++ Pascal.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the number of rows: "))
+
+for i in range(n):
+    num = 1
+
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+
+    for j in range(i + 1):
+        print(num, end="   ")
+        num = num * (i - j) // (j + 1)
+
+    print()
