c07cb4dc076e8aba3385601ef8184c27810ec59c
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-08-01 21:44:38 +0530

    35

---
+++ ReversN.py
@@ -0,0 +1,8 @@
+#Write a program to reverse a number n
+n=int(input("Enter an number: "))
+reverse=0
+while n>0:
+    digit=n%10
+    reverse=reverse*10+digit
+    n=n//10
+print("The reverse of the number is:", reverse)
