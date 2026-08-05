741c44b0a84c749325afc869a90c39c7b4e80efc
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-08-05 23:36:12 +0530

    43

---
+++ Showprime.py
@@ -0,0 +1,11 @@
+#Write a program to display all prime numbers from 1 to n.
+n=int(input("Enter a number: "))
+print("Prime numbers from 1 to", n, "are:") 
+for num in range(2, n + 1):
+    is_prime=True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime=False
+            break
+    if is_prime:
+        print(num)
