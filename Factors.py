98444c588be0852c842784a1dee0e7c598f4c385
Abhishek sontakke <abhisheksontakke17@gmail.com>
2026-08-13 23:25:52 +0530

    50

---
+++ Factors.py
@@ -0,0 +1,6 @@
+#Write a program to find all factors (divisors) of a number n
+n=int(input("Enter a number: "))
+print("Factors of", n, "are:")
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i)
